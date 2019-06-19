0c1544c6fe5bc26f5ceda41b5a11ba5a54e16ee5
Tetiana Farhuts <tfarguts@gmail.com>
2019-06-19 18:47:41 -0400

    Updates

---
+++ precache-manifest.de9a525497e91e00e78a6116c2f6b3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9fe8c586eaa424da50dc0cc9d8a306",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "05aea0572642034e0613",
+    "url": "/myportfolio/static/css/main.ca0e0aad.chunk.css"
+  },
+  {
+    "revision": "32c71a542c70433db409",
+    "url": "/myportfolio/static/js/2.6a085d61.chunk.js"
+  },
+  {
+    "revision": "05aea0572642034e0613",
+    "url": "/myportfolio/static/js/main.b53a104a.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "88d225d23edfa8af47b6c4be2b4dc299",
+    "url": "/myportfolio/static/media/img2.88d225d2.jpg"
+  }
+]);
